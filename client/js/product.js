ffa009b070691b5eab24c89118f5e433ed864829
Alejandro <asimonh06@gmail.com>
2025-05-02 20:11:45 -0500

    second changes

--- client/js/product.js
+++ client/js/product.js
@@ -1,6 +1,6 @@
 import { onCarts } from "./cart.js";
 
-const API_URL = "http://localhost:4000/api/products"; /* http://backend:4000/api/products => docker */
+const API_URL = "https://proyectovethub-production.up.railway.app/api/products"; /* http://backend:4000/api/products => docker */
 
 /* DOM iD */
 const $container = document.getElementById("product-container");
