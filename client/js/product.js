ec4d399919380aa6ffd5eeb887035119e710be49
Alejandro <asimonh06@gmail.com>
2025-05-02 20:32:15 -0500

    mi commit

--- client/js/product.js
+++ client/js/product.js
@@ -1,6 +1,6 @@
 import { onCarts } from "./cart.js";
 
-const API_URL = `${process.env.LINK}/api/products`
+const API_URL = "https://proyectovethub-production.up.railway.app/api/products";
 
 /* DOM iD */
 const $container = document.getElementById("product-container");
